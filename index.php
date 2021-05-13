c947154494f89c2e64e27d94cbd263400a4bb210
Ayush04u <84006889+Ayush04u@users.noreply.github.com>
2021-05-13 09:12:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 <html>
 <head>
 	<title></title>
+	<?php  include 'Link/links.php'; ?>
+	<?php  include  'Css/style.php'; ?>
+</head>
+<body>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
